2f0bc19873ed0fe44453ac39d4dd2493efcb21c7
victor <vjotaa@gmail.com>
2017-09-23 13:08:56 -0400

    getStatus Route

--- api/routes/status.js
+++ api/routes/status.js
@@ -3,5 +3,6 @@ const statusController = require("../controllers/status");
 const status = express.Router();
 const md_auth = require("../middlewares/authenticated");
 
+status.get("/obtener-status/:id?", statusController.getStatus);
 status.post("/crear-status", md_auth.ensureAuth, statusController.createStatus);
 module.exports = status;
