94e272ba15a6d8cf156ad664af516080f8d449ca
victor <vjotaa@gmail.com>
2017-10-10 19:19:13 -0400

    create edit proposal project

---
+++ src/app/proposal/proposals-project/edit-proposal-project/edit-proposal-project.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-proposal-project',
+  templateUrl: './edit-proposal-project.component.html',
+  styleUrls: ['./edit-proposal-project.component.scss']
+})
+export class EditProposalProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
