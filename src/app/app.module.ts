0bb868dbd9608d16ad14014ad48f53cedcb11af9
victor <vjotaa@gmail.com>
2017-10-10 19:24:17 -0400

    app module some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { AuthService } from './auth/auth.service';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContactModule } from 'app/contact/contact.module';
+import { ProjectModule } from 'app/project/project.module';
+import { NgsRevealModule } from 'ng-scrollreveal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgsRevealModule } from 'ng-scrollreveal';
-//Custom Module
+import { ArticleModule } from './article/article.module';
 import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
 import { HomeModule } from './home/home.module';
-import { ProjectModule } from 'app/project/project.module';
-import { ContactModule } from 'app/contact/contact.module';
+import { ProposalModule } from './proposal/proposal.module';
+
+//Custom Module
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -20,8 +23,10 @@ import { ContactModule } from 'app/contact/contact.module';
     NgsRevealModule.forRoot(),
     AuthModule,
     HomeModule,
+    ArticleModule,
     ProjectModule,
-    ContactModule
+    ContactModule,
+    ProposalModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
