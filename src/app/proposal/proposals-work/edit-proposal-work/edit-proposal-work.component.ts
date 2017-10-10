0d575f16e56576d5f75cb8e5446ec65ede391742
victor <vjotaa@gmail.com>
2017-10-10 19:18:49 -0400

    create edit work

---
+++ src/app/proposal/proposals-work/edit-proposal-work/edit-proposal-work.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-proposal-work',
+  templateUrl: './edit-proposal-work.component.html',
+  styleUrls: ['./edit-proposal-work.component.scss']
+})
+export class EditProposalWorkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
