f0a93d41c4db54a719f72c3fb1e6dd37601c42bb
victor <vjotaa@gmail.com>
2017-10-10 19:19:02 -0400

    create proposal work

---
+++ src/app/proposal/proposals-work/create-proposal-work/create-proposal-work.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-proposal-work',
+  templateUrl: './create-proposal-work.component.html',
+  styleUrls: ['./create-proposal-work.component.scss']
+})
+export class CreateProposalWorkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
